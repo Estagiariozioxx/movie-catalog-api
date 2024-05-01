4c28a0c0b13a6f04abaf188be5e3c3ba57bb840d
Leonardo Lopes <leonardolopes-@hotmail.com>
2024-05-01 17:43:40 -0300

    sdf

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MoviesModule } from './movies/movies.module'; // Importe seu módulo MoviesModule
-import { Movie } from './movies/movie.entity';
+import { MoviesModule } from './movies/movies.module';
 import { AuthModule } from './auth/auth.module';
+import { Movie } from './movies/movie.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres',
+      host: 'postgres', // Alterado para o nome do serviço do banco de dados
       port: 5432,
       username: 'postgres',
       password: 'postgres123',
       database: 'movie_catalog',
       autoLoadEntities: true,
       synchronize: true,
-    })
-    ,
+      entities: [Movie], // Adicionando a entidade Movie aqui
+    }),
     MoviesModule,
-    AuthModule, // Adicione o MoviesModule aqui
+    AuthModule,
   ],
 })
 export class AppModule {}
